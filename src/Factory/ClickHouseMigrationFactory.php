3fff19a2676e52d9fbe3e377e4e04f4c7cdcd8f3
Vitalii Zhuk <zhuk2205@gmail.com>
2025-10-09 08:31:05 +0300

    Add ClickHouse migration factory.

---
+++ src/Factory/ClickHouseMigrationFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the FiveLab Migrator package
+ *
+ * (c) FiveLab
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+declare(strict_types = 1);
+
+namespace FiveLab\Component\Migrator\Factory;
+
+use ClickHouseDB\Client;
+use FiveLab\Component\Migrator\Migration\MigrationInterface;
+use FiveLab\Component\Migrator\MigrationMetadata;
+
+readonly class ClickHouseMigrationFactory implements MigrationFactoryInterface
+{
+    public function __construct(private Client $client)
+    {
+    }
+
+    public function create(MigrationMetadata $metadata): MigrationInterface
+    {
+        return $metadata->class->newInstance($this->client);
+    }
+}
